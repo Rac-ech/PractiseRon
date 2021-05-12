dced759633a69739d05d3055cc6debfb08375df7
Rac-ech <rachannepal6@gmail.com>
2021-05-12 23:25:26 +0545

    first

---
+++ home.js
@@ -0,0 +1,31 @@
+//console.log("Hello");
+//console.log("Hello");
+//alert("yoo");
+
+//Variables
+//var a = "Smoothie";
+//console.log(a);
+
+//var num = 69;
+//console.log(num);
+
+//var age = prompt("What's your age, mate?");
+//document.getElementById("someText").innerHTML = age;
+
+//Numbers in js
+var num1 = 10;
+
+//Increment by 1
+num1++;
+
+//Decrement by 1
+num1--;
+console.log(num1);
+
+//Divide multiply or remainder
+console.log(num1 % 6);
+
+//Increment and Decrement by any number (Here by 5)
+var num2 = 20;
+num2 += 5;
+console.log(num2);
